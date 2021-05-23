9073050bcaf7fc38f996fef63132261638e9c523
Billy Dexter <billydexter15@gmail.com>
2021-05-23 13:20:01 -0500

    Edit blog from frontend

--- blog/views.py
+++ blog/views.py
@@ -21,13 +21,30 @@ def post_detail(request, pk):
 def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
-        if form.is_valid(): #Checking to see if input is valid
-            post = form.save(commit=False) #Saving the form but not adding to db
+        if form.is_valid():  # Checking to see if input is valid
+            post = form.save(commit=False)  # Saving the form but not adding to db
             post.author = request.user
             post.publish_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)#Redirects you to the post you just created
+            return redirect('post_detail', pk=post.pk)  # Redirects you to the post you just created
     else:
-            form = PostForm()
-            stuff_for_frontend = {'form': form}
+        form = PostForm()
+        stuff_for_frontend = {'form': form}
+    return render(request, 'blog/post_edit.html', stuff_for_frontend)
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        # updating existing form/post
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.publish_date = timezone.now()
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm(instance=post)# otherwise show the existing blog post
+        stuff_for_frontend = {'form': form}
     return render(request, 'blog/post_edit.html', stuff_for_frontend)
